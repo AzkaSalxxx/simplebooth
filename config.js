a3aca2488b0131a471c015ad1bb225cdd9d28a13
Azka Salxxx <ghifars96@gmail.com>
2026-07-12 21:29:01 +0700

    Update config.js

--- config.js
+++ config.js
@@ -18,9 +18,12 @@ const CONFIG={
     "6v2":{name:"Strip 6V2",photoCount:6,width:1200,height:2200}
   },
   templates:[
-    {id:"default-2",name:"Default 2",layout:"2",file:"assets/templates/default-2.png"},
-    {id:"default-3",name:"Default 3",layout:"3",file:"assets/templates/default-3.png"},
-    {id:"default-4",name:"Default 4",layout:"4",file:""},
+    {id:"f2w",name:"D2W",layout:"2",file:"assets/templates/f2w.png"},
+    {id:"f2b",name:"D2B",layout:"2",file:"assets/templates/f2b.png"},
+    {id:"f3w",name:"D3W",layout:"3",file:"assets/templates/f3w.png"},
+    {id:"f3b",name:"D3B",layout:"3",file:"assets/templates/f3b.png"},
+    {id:"f4w",name:"D4W",layout:"4",file:"assets/templates/f4w.png"},
+    {id:"f4b",name:"D4B",layout:"4",file:"assets/templates/f4b.png"},
     {id:"default-6",name:"Default 6",layout:"6",file:""},
     {id:"default-4v2",name:"Default 4V2",layout:"4v2",file:""},
     {id:"default-6v2",name:"Default 6V2",layout:"6v2",file:""},
